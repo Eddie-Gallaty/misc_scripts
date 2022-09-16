83abb6c01afa9b442515f3a0c8f7f2e0dec276af
Eddie <edward.gallaty@beaumont.org>
2022-09-16 13:26:41 -0400

    add computer objects to ad with group assigned

--- create_computer_objects.ps1
+++ create_computer_objects.ps1
@@ -6,7 +6,6 @@ $computers = Get-Content "C:\Scripts\fun_stuff\helloAD.txt"
 #loop through list of computer objects
 foreach ($pc in $computers){
     New-ADComputer -Name $pc -SAMAccountName $pc  -path "OU=MyOU,OU=Test,OU=Servers,OU=MyOrg,DC=eg,DC=helloworld,DC=org"
-    
 }
 
 #add the objects to a group
