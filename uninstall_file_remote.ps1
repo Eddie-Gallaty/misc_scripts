f1e510d8b7546b571538f45b5744190e924e4f0a
Eddie <edward.gallaty@beaumont.org>
2022-09-16 15:19:10 -0400

    initial commit for remote file install

---
+++ uninstall_file_remote.ps1
@@ -0,0 +1,2 @@
+#this will loop thru a text file for hostnames and uninstall a program called "HelloWorld" Change 'Uninstall' to 'Install' for the reverse
+Invoke-Command -Credential DOMAIN\ -ComputerName (Get-Content C:\users\user\documents\scripts\Powershell\computernames.txt) -ScriptBlock {Get-CimInstance -ClassName Win32_Product -Filter "name like '%HelloWorldt%'" | Invoke-CimMethod -MethodName Uninstall}
